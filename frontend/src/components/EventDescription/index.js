c50f11ce83b91ec4b4b5d7d477394d86376bf7da
Brian Alexis <51463930+brrianalexis@users.noreply.github.com>
2020-10-25 20:04:29 -0300

    feat: individual event page connected to API

--- frontend/src/components/EventDescription/index.js
+++ frontend/src/components/EventDescription/index.js
@@ -8,68 +8,29 @@ import {
   TableRow,
 } from '../Table';
 
-export const LeftColumn = () => {
+export const LeftColumn = ({ title, description, location }) => {
   return (
     <Flex direction='column' h='100%'>
       <Flex align='baseline'>
         <Heading size='xl' as='h1' mr={4}>
-          Event Name
+          {title}
         </Heading>
         <Heading size='md' as='h2'>
-          Event Location
+          {location}
         </Heading>
       </Flex>
       <Box>
-        <Text mt={2}>
-          Donec consectetur, ligula id maximus convallis, lacus augue cursus
-          leo, vitae porta mauris tortor non libero. Duis ultricies ante vel
-          arcu suscipit, ut consectetur mi mattis. Pellentesque consequat
-          interdum libero, ac rutrum purus congue ut. Nulla eu lorem quis ex
-          interdum lobortis. Aliquam at mauris ex. Ut nisl lorem, bibendum ac
-          sem vel, fermentum porta tortor. Vestibulum vulputate magna at
-          porttitor laoreet. Nulla volutpat magna ut sapien aliquet, sit amet
-          efficitur felis posuere. Aenean lorem magna, bibendum eu nisl vel,
-          placerat ullamcorper nibh. Nam aliquam venenatis nunc. Nam consequat,
-          orci vel egestas venenatis, nulla nunc sollicitudin lectus, a feugiat
-          risus orci et ligula. Phasellus at viverra erat, sit amet ultricies
-          lorem. Integer at est eget magna egestas vestibulum.
-        </Text>
-        <Text mt={2}>
-          Nunc nec est quis dolor sodales euismod et non diam. Donec ligula
-          nunc, efficitur vitae turpis et, suscipit bibendum ipsum. Nulla a
-          finibus nibh. Praesent sagittis felis a tempus vehicula. Sed orci
-          velit, molestie sit amet bibendum sed, scelerisque ac ante. Mauris
-          blandit sem nisl, a porta lacus commodo in. Vestibulum dui nunc,
-          aliquam sed luctus id, dictum vel sapien. Maecenas viverra interdum
-          mauris, vestibulum sagittis leo. Proin eu sodales nunc. Integer congue
-          orci in tempor maximus. Praesent luctus augue tellus, in dignissim
-          mauris gravida sit amet. Maecenas quis ultricies ipsum, laoreet
-          convallis risus.
-        </Text>
-        <Text mt={2}>
-          Proin volutpat quis tortor porttitor condimentum. Maecenas ac enim
-          magna. Ut convallis purus vel feugiat cursus. Aliquam erat volutpat.
-          Nam quis libero elementum, sollicitudin quam ut, euismod elit. Donec
-          maximus porta ipsum, ut suscipit risus ullamcorper tincidunt. Nullam
-          dapibus diam vitae purus malesuada auctor. Fusce nec molestie eros.
-          Maecenas pretium lacus mauris, nec pellentesque felis cursus eu.
-          Suspendisse mollis massa vel mauris egestas, quis posuere neque
-          luctus. Maecenas in egestas libero. Fusce tellus ante, mollis vitae
-          augue id, viverra blandit mauris. Pellentesque commodo sapien ac
-          aliquet hendrerit. Duis laoreet, justo eu mattis sagittis, sem erat
-          consequat sapien, eget fermentum felis metus vitae justo. Pellentesque
-          accumsan consequat felis non hendrerit.
-        </Text>
+        <Text mt={2}>{description}</Text>
       </Box>
     </Flex>
   );
 };
 
-export const RightColumn = () => {
+export const RightColumn = ({ image, dates }) => {
   return (
     <Flex direction='column' align='center' justify='space-around' h='100%'>
-      <Image fallbackSrc='https://via.placeholder.com/250' />
-      <Table border='1px'>
+      <Image src={image} fallbackSrc='https://via.placeholder.com/400' mb={4} />
+      <Table border='1px' mb={4}>
         <TableHead>
           <TableRow>
             <TableHeader>Date</TableHeader>
@@ -78,21 +39,13 @@ export const RightColumn = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          <TableRow>
-            <TableCell>20 Jul</TableCell>
-            <TableCell>19:30</TableCell>
-            <TableCell>$200</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>20 Jul</TableCell>
-            <TableCell>19:30</TableCell>
-            <TableCell>$200</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>20 Jul</TableCell>
-            <TableCell>19:30</TableCell>
-            <TableCell>$200</TableCell>
-          </TableRow>
+          {dates?.map(date => (
+            <TableRow key={date}>
+              <TableCell>{date.split(' ')[0]}</TableCell>
+              <TableCell>{date.split(' ')[1]}</TableCell>
+              <TableCell>$200</TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </Flex>
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,15 +1,42 @@
-import { Grid } from '@chakra-ui/core';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Axios from 'axios';
+import { Flex, Grid, Spinner } from '@chakra-ui/core';
 import { RiSave3Fill } from 'react-icons/ri';
 import { LeftColumn, RightColumn } from '../components/EventDescription';
 import { FixedButton } from '../components/FixedBRButton';
 
 export const EventDetail = () => {
+  const { id } = useParams();
+  const [event, setEvent] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async function getEvent() {
+      const {
+        data: { event: eventById },
+      } = await Axios.get(`http://localhost:8000/events/${id}`);
+      setEvent(eventById);
+      setLoading(false);
+    })();
+  }, [id]);
+
   return (
     <>
-      <Grid templateColumns='70% 30%' minH='80vh' mx={4}>
-        <LeftColumn />
-        <RightColumn />
-      </Grid>
+      {loading ? (
+        <Flex align='center' justify='center' h='70vh'>
+          <Spinner size='xl' color='teal.500' />
+        </Flex>
+      ) : (
+        <Grid templateColumns='70% 30%' minH='80vh' mx={4}>
+          <LeftColumn
+            title={event.title}
+            description={event.description}
+            location={event.location}
+          />
+          <RightColumn image={event.eventImage} dates={event.dates} />
+        </Grid>
+      )}
       <FixedButton icon={RiSave3Fill} label='Save event' />
     </>
   );
