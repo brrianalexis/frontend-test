2a69003575d91cbf6cf2fea06190caa23ccc8b1d
Brian Alexis <51463930+brrianalexis@users.noreply.github.com>
2020-10-25 14:08:43 -0300

    style: prettier format

--- backend/test/index.js
+++ backend/test/index.js
@@ -3,22 +3,29 @@ var test = require('tape');
 var request = require('supertest');
 var app = require('../app/server');
 
-var expectedEvents = {events: [ require('../app/db/events/1.json'), require('../app/db/events/2.json') ]}
-var expectedEvent = {event: require('../app/db/events/2.json')};
-var expectedFeaturedEvents = {events: [ require('../app/db/events/2.json') ] };
-var postEvent = {
-  "title": "24 Festival de Cine para Niños y Jóvenes – Divercine",
-  "eventImage": "http://www.cartelera.com.uy/imagenes_espectaculos/moviedetail13/17511.jpg",
-  "description": "Del lunes 27 de julio al sábado 1º de agosto se desarrolla en el Auditorio del SODRE Nelly Goitiño (18 de Julio y Rio Branco) el 24 Festival de Cine para Niños y Jóvenes - Divercine. La programación, que incluye películas de largo, medio y cortometraje de varias partes del mundo, está dividida en tres franjas de horarios cada día, de acuerdo a las edades del público a las que van dirigidas: a las 13 horas a partir de 3 años de edad; a las 14 horas a partir de los 6 años de edad; y desde las 15 se programan los medio y largometrajes para niños más grandes y adolescentes.",
-  "dates": [
-    "07/27/2015 13:00",
-    "07/28/2015 13:00",
-    "07/29/2015 13:00",
-    "07/30/2015 13:00",
-    "07/31/2015 13:00",
-    "07/1/2015 13:00"
+var expectedEvents = {
+  events: [
+    require('../app/db/events/1.json'),
+    require('../app/db/events/2.json'),
   ],
-  "location": "Auditorio del SODRE"
+};
+var expectedEvent = { event: require('../app/db/events/2.json') };
+var expectedFeaturedEvents = { events: [require('../app/db/events/2.json')] };
+var postEvent = {
+  title: '24 Festival de Cine para Niños y Jóvenes – Divercine',
+  eventImage:
+    'http://www.cartelera.com.uy/imagenes_espectaculos/moviedetail13/17511.jpg',
+  description:
+    'Del lunes 27 de julio al sábado 1º de agosto se desarrolla en el Auditorio del SODRE Nelly Goitiño (18 de Julio y Rio Branco) el 24 Festival de Cine para Niños y Jóvenes - Divercine. La programación, que incluye películas de largo, medio y cortometraje de varias partes del mundo, está dividida en tres franjas de horarios cada día, de acuerdo a las edades del público a las que van dirigidas: a las 13 horas a partir de 3 años de edad; a las 14 horas a partir de los 6 años de edad; y desde las 15 se programan los medio y largometrajes para niños más grandes y adolescentes.',
+  dates: [
+    '07/27/2015 13:00',
+    '07/28/2015 13:00',
+    '07/29/2015 13:00',
+    '07/30/2015 13:00',
+    '07/31/2015 13:00',
+    '07/1/2015 13:00',
+  ],
+  location: 'Auditorio del SODRE',
 };
 
 test('GET /events support CORS', function (t) {
@@ -76,7 +83,7 @@ test('GET /events/:id returns the correct event', function (t) {
     });
 });
 
-test('GET /events/:id returns 404 when the event doesn\'t exists', function (t) {
+test("GET /events/:id returns 404 when the event doesn't exists", function (t) {
   request(app)
     .get('/events/999')
     .expect('Content-Type', /json/)
@@ -90,12 +97,12 @@ test('POST /events creates an event and returns it with an id', function (t) {
   request(app)
     .post('/events')
     .set('Accept', 'application/json')
-    .send({event: postEvent})
+    .send({ event: postEvent })
     .end(function (err, res) {
       t.error(err, 'No error');
 
       postEvent.id = 3;
-      t.same(res.body, {event: postEvent});
+      t.same(res.body, { event: postEvent });
 
       fs.unlinkSync(__dirname + '/../app/db/events/3.json');
 
