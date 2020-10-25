495a6bdf01ef9dc76eeeb4d7f6af265d36514f21
Brian Alexis <51463930+brrianalexis@users.noreply.github.com>
2020-10-25 00:54:01 -0300

    feat: bottom right corner fixed button

---
+++ frontend/src/components/FixedBRButton/index.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import { Flex, IconButton, Tooltip } from '@chakra-ui/core';
+
+export const FixedButton = ({ icon, label }) => {
+  return (
+    <Flex
+      position='fixed'
+      justify='center'
+      align='center'
+      bottom={10}
+      right={10}
+    >
+      <Tooltip
+        position='fixed'
+        bottom={5}
+        right={5}
+        mt={8}
+        size='lg'
+        label={label}
+        placement='left'
+      >
+        <IconButton
+          mb={2}
+          bg='transparent'
+          size='lg'
+          aria-label={label}
+          as={icon}
+          isRound
+        />
+      </Tooltip>
+    </Flex>
+  );
+};
+
+FixedButton.propTypes = {
+  icon: PropTypes.element.isRequired,
+  label: PropTypes.string.isRequired,
+};
